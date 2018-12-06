b8009097b0ac8904359e8c6665fec5363820091f
Kewyn Medina <kamzprl07@gmail.com>
2018-12-06 11:44:57 -0600

    test 3

--- src/components/hero/heroComponent.js
+++ src/components/hero/heroComponent.js
@@ -7,7 +7,7 @@ class HeroComponent extends Component {
       <section className="main-section hero-header section--with-backdrop section--with-parallax">
         <h2 className="hero-header__subtitle">Hello there! I am</h2>
         <h1 className="hero-header__title">Kewyn Medina</h1>
-        <h2 className="hero-header__subtitle">Technology Specialist</h2>
+        <h2 className="hero-header__subtitle">Technology Specialist, HN</h2>
     </section>
     );
   }
